7ffb2aa7645849929daaa6631427096418515827
balabobo <digoo2008@gmail.com>
2013-12-30 11:27:29 +0800

    测试
    测试

---
+++ 原型文件/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l),_(c,m,e,f,g,n),_(c,o,e,f,g,p,q,[_(c,r,e,f,g,s),_(c,t,e,f,g,u)]),_(c,v,e,f,g,w),_(c,x,e,f,g,y)]);}; 
+var b="rootNodes",c="pageName",d="主页",e="type",f="Wireframe",g="url",h="主页.html",i="余额支付",j="余额支付.html",k="信用卡快捷支付",l="信用卡快捷支付.html",m="酷开支付密码找回",n="酷开支付密码找回.html",o="会员中心-支付安全置",p="会员中心-支付安全置.html",q="children",r="支付 密码设置2",s="支付_密码设置2.html",t="支付密码 找回",u="支付密码_找回.html",v="会员中心",w="会员中心.html",x="账号管理",y="账号管理.html";
+return _creator();
+})();
